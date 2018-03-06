e0e32af24afa5b698996d655789a2b7a168b2d3b
Jonathan Wesley Vicente <hello@jonathanvicente.pt>
2018-03-06 19:33:06 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next)=>{
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  
+  fs.appendFile('server.log', log + '\n', (err)=>{
+    if(err)
+      console.log(err.message);
+  });
+  
+  next();
+});
+
+hbs.registerHelper('currentYear', () => new Date().getFullYear());
+hbs.registerHelper('upperCase', (text="") => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/json', (req, res) => {
+  res.send({
+    title: 'Title',
+    body: 'Content',
+    data: {
+      'info': 'content'
+    }
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage:'Unable to handle request'
+  });
+});
+
+
+app.listen(3000, ()=>{
+  console.log('Server is up on port 3000');
+  
+});
