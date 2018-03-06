488940390a230e1c6eae0e7519ceea6cdfee6b9e
Jonathan Wesley Vicente <hello@jonathanvicente.pt>
2018-03-06 21:32:50 +0000

    Add projects

--- server.js
+++ server.js
@@ -38,6 +38,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
+
 app.get('/json', (req, res) => {
   res.send({
     title: 'Title',
